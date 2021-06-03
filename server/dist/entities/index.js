e24604e2225a8caa7cf6694ef30c07665b75f33f
Tim <timjang3@gmail.com>
2021-06-03 12:37:42 -0700

    change

---
+++ server/dist/entities/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Videos_1 = require("./Videos");
+Object.defineProperty(exports, "Videos", { enumerable: true, get: function () { return Videos_1.Videos; } });
+var UserAccount_1 = require("./UserAccount");
+Object.defineProperty(exports, "UserAccount", { enumerable: true, get: function () { return UserAccount_1.UserAccount; } });
+//# sourceMappingURL=index.js.map
