1feff497d6d68076f32e6d4a4c1485c40af8a65d
braydenwerner <braydenwerner0@gmail.com>
2021-06-12 17:51:10 -0700

    update

--- client/src/config/config.ts
+++ client/src/config/config.ts
@@ -2,8 +2,8 @@ const dev = process.env.NODE_ENV !== 'production'
 
 export const URL = dev
   ? 'http://localhost:3000/api'
-  : 'https://thumbnail-game.vercel.app/'
+  : 'https://thumbnail-game.vercel.app/api'
 
 export const serverURL = dev
   ? 'http://localhost:4000'
-  : 'https://wern-fullstack-template.herokuapp.com/'
+  : 'https://thumbnail-game.herokuapp.com/'
