ef977a4afda1a2465d211cd83b3c8ced9b4f1e5a
timjang3@gmail.com <timjang3@gmail.com>
2021-06-12 19:17:24 -0700

    psuh

--- client/src/components/elements/MobileNotSupported/MobileNotSupported.tsx
+++ client/src/components/elements/MobileNotSupported/MobileNotSupported.tsx
@@ -1,8 +1,3 @@
 export const MobileNotSupported: React.FC = () => {
-<<<<<<< HEAD
-    return <div>Devices with this screen size are currently not supported!</div>
+  return <div>Devices with this screen size are currently not supported!</div>
 }
-=======
-  return <div>Mobile not supported</div>
-}
->>>>>>> f704991f986fd993a1fcf4e71565f0108d069522
