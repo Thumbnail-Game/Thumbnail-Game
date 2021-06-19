982e7aefa79da7b07009e287da6502f1a1d637e4
Brayden Werner <braydenwerner0@gmail.com>
2021-06-19 10:13:06 -0700

    level calculations

--- client/src/components/elements/MainProfile/MainProfile.tsx
+++ client/src/components/elements/MainProfile/MainProfile.tsx
@@ -1,7 +1,7 @@
+import { useState, useEffect } from 'react'
 import {
   GetGamesByUserQuery,
   GetUserByDisplayNameQuery,
-  useGetGamesByUserQuery,
 } from '../../../generated/graphql'
 import * as Styled from './MainProfile.styled'
 
@@ -14,7 +14,24 @@ export const MainProfile: React.FC<ProfileChartProps> = ({
   userData,
   gamesData,
 }) => {
-  console.log(gamesData)
+  const [level, setLevel] = useState<number>(0)
+  const [percent, setPercent] = useState<number>(0)
+
+  useEffect(() => {
+    //  calculate level by total score
+    if (gamesData && gamesData.getGamesByUser) {
+      let totalScore = 0
+      for (const gameObj of gamesData.getGamesByUser) {
+        totalScore += gameObj.score
+      }
+
+      //  10 points of exp for level
+      setPercent(
+        (Math.abs(totalScore / 10) - Math.floor(totalScore / 10)) * 100
+      )
+      setLevel(totalScore / 10)
+    }
+  }, [gamesData])
 
   return (
     <Styled.Wrapper>
@@ -22,7 +39,14 @@ export const MainProfile: React.FC<ProfileChartProps> = ({
         <Styled.Name>{userData!.userByDisplayName!.displayName}</Styled.Name>
       </Styled.LeftContainer>
       <Styled.MiddleContainer></Styled.MiddleContainer>
-      <Styled.RightContainer></Styled.RightContainer>
+      <Styled.RightContainer>
+        {level && <div>{Math.floor(level)}</div>}
+        {percent && (
+          <div>
+            {percent}% to level {Math.floor(level) + 1}
+          </div>
+        )}
+      </Styled.RightContainer>
     </Styled.Wrapper>
   )
 }
