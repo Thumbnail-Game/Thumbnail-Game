29f1ed3af101872fa1a7078550a81f6ab59e8e68
Brayden Werner <braydenwerner0@gmail.com>
2021-06-19 21:34:38 -0700

    removed profile page

--- client/src/pages/profile.tsx
+++
@@ -1,7 +0,0 @@
-interface ProfileProps { }
-
-export const Profile: React.FC<ProfileProps> = ({ }) => {
-  // get the user data
-  //  three main components, fetch data here based on userid
-  return <div>Profile Page</div>
-}
