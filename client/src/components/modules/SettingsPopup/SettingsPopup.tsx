c714c9bf4684f12f565ec8f6a36da87d00b8381a
Brayden Werner <braydenwerner0@gmail.com>
2021-06-20 21:05:38 -0700

    removed bad videos

--- client/src/components/modules/SettingsPopup/SettingsPopup.tsx
+++ client/src/components/modules/SettingsPopup/SettingsPopup.tsx
@@ -1,17 +1,10 @@
-import { useContext } from 'react'
-
-import { ThemeContext } from '../../../providers/AppProvider'
 import { ThemeToggle } from '../../elements/index'
 import * as Styled from './SettingsPopup.styled'
 
 export const SettingsPopup: React.FC = () => {
-  const { themeMode } = useContext(ThemeContext)
-
   return (
     <>
-      <Styled.TriangleIcon
-        size={50}
-      />
+      <Styled.TriangleIcon size={50} />
       <Styled.SettingsContainer>
         <ThemeToggle />
         <Styled.ThemeLabel>Dark Mode</Styled.ThemeLabel>
