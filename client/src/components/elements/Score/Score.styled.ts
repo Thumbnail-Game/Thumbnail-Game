6a240630132bcf3a17cfe2125014aa540e591aee
Brayden Werner <braydenwerner0@gmail.com>
2021-06-22 00:19:13 -0700

    Added colored container back to score

--- client/src/components/elements/Score/Score.styled.ts
+++ client/src/components/elements/Score/Score.styled.ts
@@ -20,18 +20,15 @@ export const ScoreWrapper = styled.div`
   -moz-user-select: none; /* Firefox */
   -ms-user-select: none; /* IE10+/Edge */
   user-select: none; /* Standard */
-<<<<<<< HEAD
   background-color: ${(props) => props.theme.scoreBackground};
-=======
 
   @media (max-width: 760px) {
-    transform:scale(1.2);
-    width:240px;
+    transform: scale(1.2);
+    width: 240px;
     margin-top: 10px;
-    flex-direction:column;
-    justify-content:space-between;
+    flex-direction: column;
+    justify-content: space-between;
   }
->>>>>>> 41ea9694c1e57f75b97bc8b2c2a90b7be44d5339
 `
 export const CurrentScore = styled.div`
   font-size: 28px;
@@ -51,6 +48,6 @@ export const HighScore = styled.div`
   user-select: none; /* Standard */
 
   @media (max-width: 760px) {
-    font-size:24px;
+    font-size: 24px;
   }
 `
