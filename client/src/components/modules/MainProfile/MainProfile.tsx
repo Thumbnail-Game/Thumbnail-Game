e1c469f918c88f171c2d99e88e337f0d179a3d3e
Tim Jang <timjang3@gmail.com>
2021-06-24 16:50:49 -0700

    material ui :(

--- client/src/components/modules/MainProfile/MainProfile.tsx
+++ client/src/components/modules/MainProfile/MainProfile.tsx
@@ -8,7 +8,7 @@ import {
 import { Achievements } from '../../elements/index'
 import * as Styled from './MainProfile.styled'
 import Moment from 'moment'
-import Skeleton from 'react-loading-skeleton';
+import Skeleton from '@material-ui/lab/Skeleton';
 
 interface ProfileChartProps {
   userData: GetUserByDisplayNameQuery
@@ -110,7 +110,7 @@ export const MainProfile: React.FC<ProfileChartProps> = ({
         Toggle Gamemode
       </Styled.Toggle>
       <Styled.LeftContainer>
-        <Styled.Name>{userData?.userByDisplayName?.displayName || <Skeleton />}</Styled.Name>
+        <Styled.Name>{userData?.userByDisplayName?.displayName || <Skeleton variant="text" />}</Styled.Name>
         {userData && userData.userByDisplayName && (
           <Styled.AccountCreatedDate>
             User since{' '}
@@ -122,7 +122,7 @@ export const MainProfile: React.FC<ProfileChartProps> = ({
           </Styled.AccountCreatedDate>
         )}
         <Styled.GamemodeTitle>
-          {gamemode.charAt(0).toUpperCase() + gamemode.slice(1) || <Skeleton />}
+          {gamemode.charAt(0).toUpperCase() + gamemode.slice(1) || <Skeleton variant="text" />}
         </Styled.GamemodeTitle>
       </Styled.LeftContainer>
       <Styled.MiddleContainer>
@@ -133,7 +133,7 @@ export const MainProfile: React.FC<ProfileChartProps> = ({
               : false
           }
         >
-          {percentile || <Skeleton />}
+          {percentile || <Skeleton variant="text" />}
         </Styled.Percentile>
         <Styled.PercentileLabel>percentile</Styled.PercentileLabel>
       </Styled.MiddleContainer>
@@ -150,11 +150,11 @@ export const MainProfile: React.FC<ProfileChartProps> = ({
                 height="10px"
               />
               {percent && (
-                <Styled.PercentText>{Math.floor(percent) || <Skeleton />}%</Styled.PercentText>
+                <Styled.PercentText>{Math.floor(percent) || <Skeleton variant="text" />}%</Styled.PercentText>
               )}
             </Styled.ProgressContainer>
             <Styled.LevelCircle>
-              {level && <div>{Math.floor(level) || <Skeleton />}</div>}
+              {level && <div>{Math.floor(level) || <Skeleton variant="text" />}</div>}
             </Styled.LevelCircle>
           </Styled.InnerProgressRow>
         </Styled.LevelContainer>
