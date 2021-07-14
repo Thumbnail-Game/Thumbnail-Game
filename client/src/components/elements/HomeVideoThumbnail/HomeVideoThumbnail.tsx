880ecf18b897b168d7762ba05b826b694a537b5d
Brayden Werner <braydenwerner0@gmail.com>
2021-07-14 11:29:40 -0700

    Started image optimization

--- client/src/components/elements/HomeVideoThumbnail/HomeVideoThumbnail.tsx
+++ client/src/components/elements/HomeVideoThumbnail/HomeVideoThumbnail.tsx
@@ -15,9 +15,10 @@ export const HomeVideoThumbnail: React.FC<Home3DProps> = ({ video }) => {
       <Styled.VideoThumbnail>
         <Image
           src={video.thumbnail}
-          alt={`image-background-${video.title}`}
+          alt={video.title}
           width={320}
           height={180}
+          onError={(e: any) => (e.target.style.display = 'none')}
         />
       </Styled.VideoThumbnail>
     </Styled.VideoWrapper>
