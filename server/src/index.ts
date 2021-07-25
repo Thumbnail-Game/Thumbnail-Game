6e5e4333fd0f7bbf05d7184f67b95ade4b2cd89a
braydenwerner <braydenwerner0@gmail.com>
2021-07-25 16:22:29 -0700

    heroku

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,11 +14,11 @@ import { GamesResolver, UserResolver, VideoResolver } from './resolvers/index'
 import { updateAllVideoViews } from './utils/updateAllVideoViews'
 
 const main = async () => {
-  console.log(process.env.HEROKU_POSTGRESQL_PURPLE_URL)
+  console.log(process.env.DATABASE_URL)
 
   const conn = await createConnection({
     type: 'postgres',
-    url: process.env.HEROKU_POSTGRESQL_PURPLE_URL,
+    url: process.env.DATABASE_URL,
     logging: true,
     synchronize: true,
     entities: [UserAccount, Videos, Games],
