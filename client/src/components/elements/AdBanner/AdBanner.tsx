9d4c784b710adfb2ab0c939ab4cacfdad925ee6f
Tim Jang <timjang3@gmail.com>
2022-05-15 13:49:32 +0900

    ad banner styling test

--- client/src/components/elements/AdBanner/AdBanner.tsx
+++ client/src/components/elements/AdBanner/AdBanner.tsx
@@ -1,13 +1,39 @@
 import { useEffect } from 'react'
 
 export const AdBanner = () => {
+  // useEffect(() => {
+  //   try {
+  //     // ;((window as any).adsbygoogle = (window as any).adsbygoogle || []).push(
+  //     //   {}
+  //     // )
+  //     ((window as any).adsbygoogle = window.adsbygoogle || []).push({});
+  //   } catch (err) {
+  //     console.log(err)
+  //   }
+  // }, [])
+
   useEffect(() => {
-    try {
-      ;((window as any).adsbygoogle = (window as any).adsbygoogle || []).push(
-        {}
-      )
-    } catch (err) {
-      console.log(err)
+    const pushAd = () => {
+      try {
+        const adsbygoogle = (window as any).adsbygoogle
+        console.log({ adsbygoogle })
+        adsbygoogle.push({})
+      } catch (e) {
+        console.error(e)
+      }
+    }
+
+    let interval = setInterval(() => {
+      // Check if Adsense script is loaded every 300ms
+      if ((window as any).adsbygoogle) {
+        pushAd()
+        // clear the interval once the ad is pushed so that function isn't called indefinitely
+        clearInterval(interval)
+      }
+    }, 300)
+
+    return () => {
+      clearInterval(interval)
     }
   }, [])
 
@@ -16,17 +42,7 @@ export const AdBanner = () => {
       className="adsbygoogle adbanner-customize"
       data-ad-client="ca-pub-6724421299859990"
       data-ad-slot="8296324938"
-      style={{
-        display: 'block',
-        position: 'absolute',
-        width: '728px',
-        height: '90px',
-        margin: 'auto',
-        top: '0',
-        bottom: '0',
-        right: '0',
-        left: '0',
-      }}
+      style={{ display: 'block', width: '300px', height: '250px' }}
     />
   )
 }
